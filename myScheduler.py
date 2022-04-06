9961ebc0257109eb20a4ba038cada4fa1c7c8021
Antonio Gonzalez <antonio.gonzalez.puertas@gmail.com>
2022-04-06 21:25:41 +0200

    correction bug tasks

--- myScheduler.py
+++ myScheduler.py
@@ -6,10 +6,10 @@ import json
 class Scheduler():
     def __init__(self,mySC):
         self.mySC = mySC
-        self.loadSchedule()
         self.dayTask=None
         self.nightTask=None
-        # schedule.every(10).seconds.do(self.schedule_message,msg="hola").tag("title")
+        self.launchSchedule()
+    # schedule.every(10).seconds.do(self.schedule_message,msg="hola").tag("title")
         # schedule.clear('title')
         #exec("schedule.every(10).seconds.do(self.reminder1)")
         pass
@@ -25,10 +25,10 @@ class Scheduler():
         print("Start night schedule:")
         if self.dayTask != None:
             print("Cancel day schedule")
-        schedule.cancel_job(self.dayTask)
+            schedule.cancel_job(self.dayTask)
         self.nightTask= schedule.every(self.mySC.TIME_LAPSE_NIGHT_HOURS).hours.do(self.mySC.take_picture)
 
-    def launchSchedule(self,data):
+    def launchSchedule(self):
         self.dayTask= schedule.every(self.mySC.TIME_LAPSE_DAY_MINUTES).minutes.do(self.mySC.take_picture)
         schedule.every().day.at(self.mySC.MORNING).do(self.startDay)
         schedule.every().day.at(self.mySC.NIGHT).do(self.endDay)
